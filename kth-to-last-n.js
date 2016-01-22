e3a9c765c7c77a7deb9f4b584a46c7b4cb3bf38c
= <kristoffer@kristofferhebert.com>
2016-01-21 17:56:37 -0800

    adding kth-to-last-n.js

---
+++ kth-to-last-n.js
@@ -0,0 +1,35 @@
+
+function LinkedListNode(value){
+    return {
+        value: value,
+        next: null
+    }
+}
+
+function kth_to_last_node(int, node){
+    var prev = []
+    var nextNode = node.next
+    var result
+
+    while(nextNode.next !== null) {
+        prev.push(nextNode)
+        nextNode = nextNode.next
+    }
+
+    result = prev[(prev.length + 1) - int]
+    return (result) ? result.value : null
+
+}
+
+var a = LinkedListNode("Angel Food")
+var b = LinkedListNode("Bundt")
+var c = LinkedListNode("Cheese")
+var d = LinkedListNode("Devil's Food")
+var e = LinkedListNode("Eccles")
+
+a.next = b
+b.next = c
+c.next = d
+d.next = e
+
+console.log(kth_to_last_node(2, a))
