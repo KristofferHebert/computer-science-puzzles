76e01eca6ef7d426ff343c3a81bd38cbaba5e1cc
= <kristoffer@kristofferhebert.com>
2016-01-22 19:05:17 -0800

    finisihing closing Parenthesis

---
+++ closingParenthesis.js
@@ -0,0 +1,21 @@
+function get_closing_paren(index, str) {
+	var open_nested_parens = 0
+	var position = index + 1
+	var sentence = str.split(' ')
+	var length = str.length
+
+	while (position <= length) {
+		var char = sentence[position]
+    	if (char == '(') {
+    		open_nested_parens += 1
+    	} else if (char == ')') {
+    		if (open_nested_parens === 0) {
+    			return position
+    		} else {
+    			open_nested_parens -= 1
+    			position += 1
+    		}
+    	}
+    	return "No closing parenthesis :("
+    }
+}
